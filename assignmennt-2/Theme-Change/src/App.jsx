0f874c1026b287eb88d7c48963a8ca7c3b9476db
Nandasai8 <mamillanandasai8@gmail.com>
2025-10-07 14:56:12 +0530

    changes

--- assignmennt-2/Theme-Change/src/App.jsx
+++ assignmennt-2/Theme-Change/src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
   const [isDark, setIsDark] = useState(false);
@@ -10,16 +9,31 @@ function App() {
   const toggleTheme = () => setIsDark(!isDark);
 
   return (
-    <div className={`container ${isDark ? "dark" : "light"}`}>
-      <h1>{isDark ? "Dark Theme" : "Light Theme"}</h1>
-      <button className="toggle-btn" onClick={toggleTheme}>
-        {isDark ? "dark theme" : "light theme"}
-      </button>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, quasi itaque. Aperiam iure rerum corrupti incidunt deleniti est nostrum dolore nisi itaque? Sequi molestiae rem illo culpa impedit, aperiam explicabo!</p>
-    </div>
+    <>
+      <nav className={`navbar ${isDark ? "dark" : "light"}`}>
+        <div className="nav-left">
+          <img src='https://lms.achieversit.com/assets/images/logo.png' alt="React Logo" className="logo" />
+
+        </div>
+        <ul className="nav-links">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        <button className="toggle-btn" onClick={toggleTheme}>
+          {isDark ? "Light Mode" : "Dark Mode"}
+        </button>
+      </nav>
+      <div className={`container ${isDark ? "dark" : "light"}`}>
+        <h1>{isDark ? "Dark Theme" : "Light Theme"}</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, quasi itaque.
+          Aperiam iure rerum corrupti incidunt deleniti est nostrum dolore nisi itaque?
+          Sequi molestiae rem illo culpa impedit, aperiam explicabo!
+        </p>
+      </div>
+    </>
   );
-};
+}
 
-
-
-export default App
+export default App;
